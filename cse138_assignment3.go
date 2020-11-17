bd87f4c1341c9ee3fecf1f13791c6434a2010225
Surya Suresh <ssuresh3@ucsc.edu>
2020-11-17 12:48:15 -0800

    working on setup

--- cse138_assignment3.go
+++ cse138_assignment3.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -10,9 +11,20 @@ import (
 func main() {
 	r := mux.NewRouter()
 
-	// var VIEW string
-	// var exists bool
-	VIEW, exists = os.LookupEnv("VIEW")
+	var view string
+	var address string
+	view, _ = os.LookupEnv("VIEW")
+	address, _ = os.LookupEnv("ADDRESS")
+	nodes := strings.Split(view, ",")
+
+	//if addresss matches first ip_addr in view
+	if address == nodes[0] {
+		// myView = view{nodes: nodes}
+		// myView.initTokens()
+
+	} else {
+
+	}
 
 	http.Handle("/", r)
 	http.ListenAndServe(":13800", nil)
