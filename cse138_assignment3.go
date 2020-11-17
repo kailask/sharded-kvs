1b2215ae13b4d05ccf979301bb7c3a946c2ef08b
Surya Suresh <ssuresh3@ucsc.edu>
2020-11-17 12:16:58 -0800

    starting set up

--- cse138_assignment3.go
+++ cse138_assignment3.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
+
+	// var VIEW string
+	// var exists bool
+	VIEW, exists = os.LookupEnv("VIEW")
 
 	http.Handle("/", r)
 	http.ListenAndServe(":13800", nil)
