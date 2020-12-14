1317bf24c928095dfbdbe33276a1e5c8b6665ad2
Kailas Krivanka <38896831+kailask@users.noreply.github.com>
2020-12-14 12:29:39 -0800

    purge

--- kvs/kvs.go
+++ kvs/kvs.go
@@ -6,7 +6,6 @@ import (
 	"math/big"
 	"math/rand"
 	"sort"
-	"strconv"
 	"time"
 )
 
@@ -15,9 +14,6 @@ type KVS map[string]string
 
 //PartitionedKVS is a string:string kvs divided into a map of partitions
 type PartitionedKVS map[uint64]KVS
-
-//RemappedKVS is a PartitionedKVS mapped to different nodes and with the tokens converted to strings
-type RemappedKVS map[string]map[string]KVS
 
 //MyKVS maps token values to k:v maps
 var MyKVS = PartitionedKVS{}
@@ -30,20 +26,15 @@ const (
 
 //Token contains an ip address and value in has space
 type Token struct {
-	Endpoint string `json:"endpoint"`
-	Value    uint64 `json:"value"`
+	Shard uint64 `json:"shard"`
+	Value uint64 `json:"value"`
 }
 
 //View contains list of current nodes and their sorted tokens
 type View struct {
 	Nodes  []string `json:"nodes"`
 	Tokens []Token  `json:"tokens"`
-}
-
-//Change is the changes to a single node during a view change
-type Change struct {
-	Removed bool     `json:"removed"`
-	Tokens  []uint64 `json:"tokens,omitempty"`
+	Shards map[uint64][]string
 }
 
 //Get returns the value given the key and token
@@ -81,21 +72,6 @@ func KeyCount() int {
 	return keyCount
 }
 
-//PushKeys tries to update the KVS with the new keys. Returns error if issue
-func PushKeys(newKeys map[string]KVS) error {
-	for name, shard := range newKeys {
-		key, _ := strconv.ParseUint(name, 10, 64)
-		if partition, exists := MyKVS[key]; exists {
-			for k, v := range shard {
-				partition[k] = v
-			}
-		} else {
-			return errors.New("Partition does not exist")
-		}
-	}
-	return nil
-}
-
 //FindToken returns the token corresponding to a given key
 func (v *View) FindToken(key string) Token {
 	hash := generateHash(key)
@@ -128,152 +104,6 @@ func (v *View) ChangeView(nodes []string) (map[string]*Change, map[string]bool) 
 	return changes, addedNodes
 }
 
-//Reshard key value pairs
-func (v *View) Reshard(change Change) RemappedKVS {
-	res := make(RemappedKVS)
-
-	if change.Removed { //case 1: node is removed
-		for vNode, storage := range MyKVS {
-			for key, value := range storage {
-				newToken := v.FindToken(key)
-				res.addKeyValue(key, value, newToken)
-			}
-			delete(MyKVS, vNode)
-		}
-	} else if len(MyKVS) == 0 { //case 2: node was just added
-		for _, token := range change.Tokens {
-			MyKVS[token] = make(KVS)
-		}
-	} else { //case 3: existing node needs to repartition
-		for _, changedToken := range change.Tokens {
-			for key, value := range MyKVS[changedToken] {
-				newToken := v.FindToken(key)
-				//Reshard key only if partition has changed
-				if newToken.Value != changedToken {
-					res.addKeyValue(key, value, newToken)
-					delete(MyKVS[changedToken], key)
-				}
-			}
-		}
-	}
-
-	return res
-}
-
-//Calculate the added and removed nodes as differences between the view and a given node list
-func (v *View) calcNodeDiff(nodes []string) (map[string]bool, map[string]bool) {
-	addedNodes, removedNodes := make(map[string]bool), make(map[string]bool)
-	nodesMap := make(map[string]uint64, len(nodes))
-
-	for _, node := range v.Nodes {
-		nodesMap[node] += 2
-	}
-
-	for _, node := range nodes {
-		nodesMap[node] += 3
-	}
-
-	for node, val := range nodesMap {
-		if val == 2 {
-			removedNodes[node] = true
-		} else if val == 3 {
-			addedNodes[node] = true
-		}
-	}
-
-	return addedNodes, removedNodes
-}
-
-func (v *View) mergeTokens(addedTokens []Token, addedNodes map[string]bool, removedNodes map[string]bool) ([]Token, map[string]*Change, bool) {
-	newLength := len(v.Tokens) + len(addedTokens) - (len(removedNodes) * NumTokens)
-	changes := make(map[string]*Change)
-	tokens := make([]Token, newLength)
-
-	lastWasChanged := false
-	vIndex, aIndex := 0, 0
-	var vToken, aToken *Token
-
-	if v.Tokens != nil && len(v.Tokens) > 0 {
-		vToken = &v.Tokens[vIndex]
-	}
-
-	if len(addedTokens) > 0 {
-		aToken = &addedTokens[aIndex]
-	}
-
-	//Iterate through new tokens list each time adding smallest token from added tokens or view tokens
-	for i := 0; aToken != nil || vToken != nil; i++ {
-		if vToken != nil {
-			//Iterate through view tokens until a token is found that isn't being removed
-			for removedNodes[vToken.Endpoint] {
-				//Register change for removed token
-				if _, exists := changes[vToken.Endpoint]; !exists {
-					changes[vToken.Endpoint] = &Change{Removed: true}
-				}
-
-				vIndex++
-				if vIndex < len(v.Tokens) {
-					vToken = &v.Tokens[vIndex]
-				} else {
-					vToken = nil
-					break
-				}
-			}
-		}
-
-		//Add smallest token to new list at i from added tokens or view tokens
-		if aToken != nil && (vToken == nil || aToken.Value <= vToken.Value) {
-			//Detect collisions either between current added and view tokens or current and prev added tokens
-			if (vToken != nil && aToken.Value == vToken.Value) || (aIndex > 0 && aToken.Value == addedTokens[aIndex-1].Value) {
-				return nil, nil, true
-			}
-
-			//Register change for added token
-			addChange(changes, aToken)
-
-			//Register change for view token changed by added token
-			if i == 0 {
-				lastWasChanged = true
-			} else if !addedNodes[tokens[i-1].Endpoint] {
-				addChange(changes, &tokens[i-1])
-
-			}
-
-			tokens[i] = *aToken
-			aIndex++
-			if aIndex < len(addedTokens) {
-				aToken = &addedTokens[aIndex]
-			} else {
-				aToken = nil
-			}
-		} else if vToken != nil {
-			//If we are adding the last token to the new list we must notify it if lastWasChanged
-			if lastWasChanged && i == len(tokens)-1 {
-				addChange(changes, vToken)
-			}
-
-			tokens[i] = *vToken
-			vIndex++
-			if vIndex < len(v.Tokens) {
-				vToken = &v.Tokens[vIndex]
-			} else {
-				vToken = nil
-			}
-		}
-	}
-
-	return tokens, changes, false
-}
-
-//Register a change for a given token to a change map
-func addChange(changes map[string]*Change, t *Token) {
-	if c, exists := changes[t.Endpoint]; exists {
-		c.Tokens = append(c.Tokens, t.Value)
-	} else {
-		changes[t.Endpoint] = &Change{Tokens: []uint64{t.Value}}
-	}
-}
-
 //Generate list of random tokens given map of nodes to add
 func generateTokens(addedNodes map[string]bool) []Token {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
@@ -297,21 +127,4 @@ func generateHash(key string) uint64 {
 	hash := md5.Sum([]byte(key))
 	bigInt := new(big.Int).SetBytes(hash[8:])
 	return bigInt.Uint64() % MaxHash
-}
-
-func (res RemappedKVS) addKeyValue(key string, value string, goalNode Token) {
-	node := goalNode.Endpoint
-	partition := strconv.FormatUint(goalNode.Value, 10)
-
-	//first check if goalNode's endpoint in res
-	if _, exists := res[node]; !exists {
-		res[node] = make(map[string]KVS)
-	}
-
-	//then check if partition in node remapping
-	if _, exists := res[node][partition]; !exists {
-		res[node][partition] = make(KVS)
-	}
-
-	res[node][partition][key] = value
 }
--- server.go
+++ server.go
@@ -12,7 +12,6 @@ import (
 	"server/kvs"
 	"strconv"
 	"strings"
-	"sync"
 
 	"github.com/gorilla/mux"
 )
@@ -43,12 +42,6 @@ type setupState struct {
 //Struct containing a value used in get and set handlers
 type keyValue struct {
 	Value *string `json:"value"`
-}
-
-//Key count struct used in building response to view change
-type shardCount struct {
-	Address  string `json:"address"`
-	KeyCount int    `json:"key-count"`
 }
 
 //Registers node as joined during initial setup and ends setup if all nodes are joined
@@ -138,112 +131,6 @@ func initHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusForbidden)
 }
 
-//Get keys counts from shards needed for view change response
-func getKeyCounts() ([]shardCount, error) {
-	var wg sync.WaitGroup
-	wg.Add(len(MyView.Nodes))
-	var mutex = &sync.Mutex{}
-	shards := map[string]int{}
-	for _, node := range MyView.Nodes {
-		go getNodeKeyCount(&wg, mutex, node, shards)
-	}
-	wg.Wait()
-
-	if len(shards) == len(MyView.Nodes) {
-		shardArray := make([]shardCount, 0, len(MyView.Nodes))
-		for address, count := range shards {
-			shardArray = append(shardArray, shardCount{Address: address + ":" + Port, KeyCount: count})
-		}
-		return shardArray, nil
-	}
-
-	return nil, errors.New("Not all key counts were found")
-}
-
-//Get key count for single node after view change
-func getNodeKeyCount(wg *sync.WaitGroup, mutex *sync.Mutex, node string, shards map[string]int) {
-	defer wg.Done()
-
-	if node == MyAddress {
-		mutex.Lock()
-		shards[node] = kvs.KeyCount()
-		mutex.Unlock()
-	} else {
-		uri := fmt.Sprintf("http://%s:%s/kvs/key-count", node, Port)
-		res, err := http.Get(uri)
-		if err == nil && res.StatusCode == http.StatusOK {
-			if res.Body != nil {
-				defer res.Body.Close()
-			}
-
-			b, err := ioutil.ReadAll(res.Body)
-			if err != nil {
-				return
-			}
-
-			k := struct {
-				KeyCount int `json:"key-count"`
-			}{}
-			err = json.Unmarshal(b, &k)
-			if err != nil {
-				return
-			}
-			mutex.Lock()
-			shards[node] = k.KeyCount
-			mutex.Unlock()
-		}
-	}
-}
-
-//Notify all nodes of impending view change
-func notifyViewChanges(addedNodes map[string]bool, changes map[string]*kvs.Change) error {
-	var wg sync.WaitGroup
-	nodesAccepted := make(map[string]bool)
-	nodesNotified := 0
-	var mutex = &sync.Mutex{}
-	wg.Add(len(MyView.Nodes) - 1) //Don't need to notify myself
-
-	//Notify nodes of view change
-	for _, node := range MyView.Nodes {
-		if addedNodes[node] {
-			//If node is newly added send viewInit instead of just view
-			nodesNotified++
-			v := viewInit{View: *MyView, Changes: *changes[node]}
-			go notifyNewView(&wg, mutex, node, v, nodesAccepted)
-			delete(changes, node)
-		} else if node != MyAddress {
-			nodesNotified++
-			go notifyViewChange(&wg, mutex, node, nodesAccepted)
-		}
-	}
-	wg.Wait()
-
-	if len(nodesAccepted) == nodesNotified {
-		return nil
-	}
-	return errors.New("Not all nodes accepted view change")
-}
-
-//Propagate changes to all necessary nodes
-func propagateViewChanges(changes map[string]*kvs.Change) error {
-	var wg sync.WaitGroup
-	changesPropagated := make(map[string]bool)
-	var mutex = &sync.Mutex{}
-
-	//Propagate changes to existing and removed nodes
-	wg.Add(len(changes))
-	for node, c := range changes {
-		go propagateChange(&wg, mutex, node, *c, changesPropagated)
-	}
-
-	wg.Wait()
-
-	if len(changesPropagated) == len(changes) {
-		return nil
-	}
-	return errors.New("Not all nodes propagated changes")
-}
-
 //Makes post request to uri with given data, returns true on success
 func makePost(uri string, data interface{}) bool {
 	b, err := json.Marshal(data)
@@ -252,104 +139,6 @@ func makePost(uri string, data interface{}) bool {
 		return err == nil && res.StatusCode == http.StatusOK
 	}
 	return false
-}
-
-//Routine to notify existing node of updated view
-func notifyViewChange(wg *sync.WaitGroup, mutex *sync.Mutex, node string, nodesAccepted map[string]bool) {
-	defer wg.Done()
-
-	uri := fmt.Sprintf("http://%s:%s/kvs/int/view-change", node, Port)
-	if makePost(uri, *MyView) {
-		mutex.Lock()
-		nodesAccepted[node] = true
-		mutex.Unlock()
-	}
-}
-
-//Routine to notify new node of its initial view state
-func notifyNewView(wg *sync.WaitGroup, mutex *sync.Mutex, node string, v viewInit, nodesAccepted map[string]bool) {
-	defer wg.Done()
-
-	uri := fmt.Sprintf("http://%s:%s/kvs/int/view-change", node, Port)
-	if makePost(uri, v) {
-		mutex.Lock()
-		nodesAccepted[node] = true
-		mutex.Unlock()
-	}
-}
-
-//Routine to push reshard to changes to another node
-func pushReshard(wg *sync.WaitGroup, mutex *sync.Mutex, node string, shard map[string]kvs.KVS, successfulReshards map[string]bool) {
-	defer wg.Done()
-
-	uri := fmt.Sprintf("http://%s:%s/kvs/int/push", node, Port)
-	if makePost(uri, shard) {
-		mutex.Lock()
-		successfulReshards[node] = true
-		mutex.Unlock()
-	}
-}
-
-//Handle keys pushed to node during reshard
-func pushHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Body != nil {
-		defer r.Body.Close()
-	}
-
-	b, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	if AmActive {
-		newKeys := make(map[string]kvs.KVS)
-		err = json.Unmarshal(b, &newKeys)
-		if err != nil {
-			log.Println(err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		//Push new keys to local KVS
-		err = kvs.PushKeys(newKeys)
-		if err != nil {
-			log.Println(err)
-			w.WriteHeader(http.StatusInternalServerError)
-		} else {
-			w.WriteHeader(http.StatusOK)
-		}
-	} else {
-		w.WriteHeader(http.StatusForbidden)
-	}
-}
-
-//Routine to propagate a change to node
-func propagateChange(wg *sync.WaitGroup, mutex *sync.Mutex, node string, c kvs.Change, changesPropagated map[string]bool) {
-	defer wg.Done()
-
-	if node != MyAddress {
-		uri := fmt.Sprintf("http://%s:%s/kvs/int/reshard", node, Port)
-		if makePost(uri, c) {
-			mutex.Lock()
-			changesPropagated[node] = true
-			mutex.Unlock()
-		}
-	} else {
-		shards := MyView.Reshard(c)
-		err := executeReshards(shards)
-		if err == nil {
-			mutex.Lock()
-			changesPropagated[node] = true
-			mutex.Unlock()
-		}
-
-		if c.Removed {
-			AmActive = false
-			log.Println("Left view")
-		}
-	}
 }
 
 //Execute an internal get request to another node and return the value
@@ -428,111 +217,6 @@ func executeDelete(token kvs.Token, key string) error {
 		return nil
 	}
 	return errors.New("Node returned bad status")
-}
-
-//Execute all reshards from this node
-func executeReshards(shards kvs.RemappedKVS) error {
-	var wg sync.WaitGroup
-	wg.Add(len(shards))
-	var mutex = &sync.Mutex{}
-	successfulReshards := make(map[string]bool)
-
-	for node, shard := range shards {
-		//Push resharded keys to respective nodes
-		go pushReshard(&wg, mutex, node, shard, successfulReshards)
-	}
-
-	wg.Wait()
-
-	if len(successfulReshards) == len(shards) {
-		return nil
-	}
-	return errors.New("Not all reshards completed")
-}
-
-//Handle internal reshard post request with changes
-func reshardHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Body != nil {
-		defer r.Body.Close()
-	}
-
-	b, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	if AmActive {
-		c := kvs.Change{}
-		err = json.Unmarshal(b, &c)
-		if err != nil {
-			log.Println(err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		shards := MyView.Reshard(c)
-		err = executeReshards(shards)
-		if err != nil {
-			log.Println(err)
-			w.WriteHeader(http.StatusInternalServerError)
-		} else {
-			w.WriteHeader(http.StatusOK)
-		}
-
-		//Become inactive if removed from view
-		if c.Removed {
-			AmActive = false
-			log.Println("Left view")
-		}
-	} else {
-		w.WriteHeader(http.StatusForbidden)
-	}
-}
-
-// Handle internal view change propagation post request
-func internalViewChangeHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Body != nil {
-		defer r.Body.Close()
-	}
-
-	b, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	if AmActive {
-		//I am already part of this view
-		v := kvs.View{}
-		err = json.Unmarshal(b, &v)
-		if err != nil {
-			log.Println(err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		*MyView = v
-		w.WriteHeader(http.StatusOK)
-	} else {
-		//I am a new node
-		v := viewInit{}
-		err = json.Unmarshal(b, &v)
-		if err != nil {
-			log.Println(err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		*MyView = v.View
-		MyView.Reshard(v.Changes)
-		AmActive = true
-		w.WriteHeader(http.StatusOK)
-
-		log.Println("Joined view")
-	}
 }
 
 //Handle internal get request with token in url
@@ -623,81 +307,6 @@ func internalDeleteHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	} else {
 		w.WriteHeader(http.StatusNotFound)
-	}
-}
-
-//Handle external view change put request, node acts as coordinator
-func viewChangeHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Body != nil {
-		defer r.Body.Close()
-	}
-
-	if !AmActive {
-		w.WriteHeader(http.StatusForbidden)
-		return
-	}
-
-	b, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Println(err)
-		return
-	}
-
-	req := struct {
-		View string `json:"view"`
-	}{}
-	err = json.Unmarshal(b, &req)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Println(err)
-		return
-	}
-
-	nodes := strings.Split(req.View, ",")
-	for i, node := range nodes {
-		nodes[i] = strings.Split(node, ":")[0]
-	}
-
-	//Update my view
-	changes, addedNodes := MyView.ChangeView(nodes)
-
-	//Update other's views
-	err = notifyViewChanges(addedNodes, changes)
-	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	// Propagate view changes
-	err = propagateViewChanges(changes)
-	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	log.Println("View updated to", nodes)
-
-	//Get keys counts from shards
-	shardCounts, err := getKeyCounts()
-	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	b, err = json.Marshal(struct {
-		Message string       `json:"message"`
-		Shards  []shardCount `json:"shards"`
-	}{Message: "View change successful", Shards: shardCounts})
-	if err == nil {
-		w.WriteHeader(http.StatusOK)
-		w.Write(b)
-	} else {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Println(err)
 	}
 }
 
@@ -945,17 +554,12 @@ func main() {
 
 	//Internal endpoints
 	r.HandleFunc("/kvs/int/init", initHandler).Methods(http.MethodGet)
-	r.HandleFunc("/kvs/int/view-change", internalViewChangeHandler).Methods(http.MethodPost)
-	r.HandleFunc("/kvs/int/reshard", reshardHandler).Methods(http.MethodPost)
-	r.HandleFunc("/kvs/int/push", pushHandler).Methods(http.MethodPost)
 	r.HandleFunc("/kvs/int/{token}/{key}", internalGetHandler).Methods(http.MethodGet)
 	r.HandleFunc("/kvs/int/{token}/{key}", internalSetHandler).Methods(http.MethodPut)
 	r.HandleFunc("/kvs/int/{token}/{key}", internalDeleteHandler).Methods(http.MethodDelete)
 
 	//External endpoints
-	r.HandleFunc("/kvs/view-change", viewChangeHandler).Methods(http.MethodPut)
 	r.HandleFunc("/kvs/key-count", keyCountHandler).Methods(http.MethodGet)
-	//only key operations are affected by faults/partitions
 	r.HandleFunc("/kvs/keys/{key}", getHandler).Methods(http.MethodGet)
 	r.HandleFunc("/kvs/keys/{key}", setHandler).Methods(http.MethodPut)
 	r.HandleFunc("/kvs/keys/{key}", deleteHandler).Methods(http.MethodDelete)
